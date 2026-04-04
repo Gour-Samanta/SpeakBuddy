d9fc6f69cf3abb7119791203e60c6512bc398b83
Gour Samanta <samantagour913@gmail.com>
2026-04-05 04:19:07 +0530

    fixed reject call

--- frontend/src/Dashboard/IncomingCall.jsx
+++ frontend/src/Dashboard/IncomingCall.jsx
@@ -1,34 +1,43 @@
 import "./callpage.css";
-import Button from '@mui/material/Button';
-import CallIcon from '@mui/icons-material/Call';
-import CallEndIcon from '@mui/icons-material/CallEnd';
+import Button from "@mui/material/Button";
+import CallIcon from "@mui/icons-material/Call";
+import CallEndIcon from "@mui/icons-material/CallEnd";
 import { useState } from "react";
-import {cleanUp, rejectCall} from "../WebRTC/webrtc.js";
+import { cleanUp, rejectCall } from "../WebRTC/webrtc.js";
 
+export default function IncomingCall({
+  incomingCall,
+  receiveCall,
+  setIncomingCall,
+}) {
+  //callId , fromUserId, offer
+ 
 
-export default function IncomingCall({incomingCall , receiveCall, setIncomingCall}){      //callId , from , offer
+  const [callPopup, setCallPopup] = useState(true);
 
-    const [callPopup , setCallPopup] = useState(true);
-
-    const declineCall=()=>{
-        rejectCall(incomingCall.callId , incomingCall.from);
-        cleanUp();
-        setIncomingCall(null);
-        setCallPopup(false);
-        
-        
-    }
-    return(
-        <> 
-        {callPopup? 
-         <div className="incoming-call-notify">
-            <h3 style={{fontWeight:"400"}}>Talkmate calling...</h3>
-            <div>
-            <Button variant="contained" color="error" onClick={declineCall}><CallEndIcon/></Button> &nbsp;&nbsp;
-            <Button variant="contained" color="success" onClick={receiveCall}><CallIcon/></Button>
-            </div>
-        </div> : null}
-        </>
-      
-    )
+  const declineCall = () => {
+    console.log("xxx ", incomingCall);
+    rejectCall(incomingCall.callId, incomingCall.fromUserId);
+    cleanUp();
+    setIncomingCall(null);
+    setCallPopup(false);
+  };
+  return (
+    <>
+      {callPopup ? (
+        <div className="incoming-call-notify">
+          <h3 style={{ fontWeight: "400" }}>Talkmate calling...</h3>
+          <div>
+            <Button variant="contained" color="error" onClick={declineCall}>
+              <CallEndIcon />
+            </Button>{" "}
+            &nbsp;&nbsp;
+            <Button variant="contained" color="success" onClick={receiveCall}>
+              <CallIcon />
+            </Button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
 }
